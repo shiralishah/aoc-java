8c488a4128dd202af0a9aae59e1db84bdbf7f70c
Shirali Shah <shiralishah1198@gmail.com>
2024-12-01 19:24:04 -0800

    setup day 02

---
+++ src/adventofcode/aoc2024/Day02.java
@@ -0,0 +1,25 @@
+package adventofcode.aoc2024;
+
+import adventofcode.InputReader;
+
+public class Day02 {
+
+    public static void main(String[] args) {
+        Day02 solution = new Day02();
+        String fileSource = "test.txt";
+//        System.out.println(solution.part1(fileSource));
+//        System.out.println(solution.part2(fileSource));
+
+    }
+
+    public void part1(String fileSource) {
+        InputReader.readLines(fileSource, this.getClass());
+        return;
+    }
+
+    public void part2(String fileSource) {
+        InputReader.readLines(fileSource, this.getClass());
+        return;
+    }
+
+}
